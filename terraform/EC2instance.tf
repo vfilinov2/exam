924d6e01849e010a8dd1ec7ac30b65ca350b3dce
vfilinov <vfilinov@yandex.ru>
2021-05-17 03:26:12 +0300

    3

--- terraform/EC2instance.tf
+++ terraform/EC2instance.tf
@@ -9,10 +9,16 @@ terraform {
 
 provider "aws" {
   region  = "us-east-2"
-  version = "~> 2.0"
 }
 
 resource "aws_instance" "build" {
+   count         = 2
+   name  = lookup(var.name-instance, count.index + 1) 
    ami           = "ami-08962a4068733a2b6"
    instance_type = "t2.micro"
+   key_name      = "aws_key"
+  
+    tags = {
+    name  = lookup(var.name-instance, count.index + 1)
+  }
  }
